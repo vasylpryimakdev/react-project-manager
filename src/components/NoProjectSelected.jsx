3161c2391a09f949dec935656dd72342a4b03a84
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-18 01:44:38 +0200

    Added project state management and project creation flow

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,6 +1,7 @@
-import noProjectImage from '../assets/no-projects.png';
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button.jsx";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -13,6 +14,9 @@ export default function NoProjectSelected() {
       </h2>
       <p className="text-stone-400 mb-4">
         Select a project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
