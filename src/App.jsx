540fc831aebfd1c83bf4aaef1c5f85e066fdbe84
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-18 13:08:44 +0200

    handleDeleteProject function created

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,18 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId,
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId,
   );
