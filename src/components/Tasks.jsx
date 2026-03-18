a636a67f99f701a8618b469097721ac2c7033073
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-18 13:17:52 +0200

    Created base Tasks component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,12 @@
+export default function Tasks({ tasks, onAdd, onDelete }) {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      NEW TASK
+      <p className="text-stone-800 my-4">
+        This project does not have any tasks yet.
+      </p>
+      <ul className="p-4 mt-8 rounded-md bg-stone-100"></ul>
+    </section>
+  );
+}
